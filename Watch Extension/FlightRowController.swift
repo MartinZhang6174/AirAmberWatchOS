20a19787b80cf81c5baf66dff7826fcf0e7baee9
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-08-21 15:00:34 -0700

    First commit, big commit, should avoid in future. Finished first 2 parts of tutorial.

---
+++ Watch Extension/FlightRowController.swift
@@ -0,0 +1,37 @@
+//
+//  FlightRowController.swift
+//  AirAber
+//
+//  Created by Martin Zhang on 2016-08-20.
+//  Copyright © 2016 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+
+class FlightRowController: NSObject {
+
+    @IBOutlet var separator: WKInterfaceSeparator!
+    @IBOutlet var originLabel: WKInterfaceLabel!
+    @IBOutlet var destinationLabel: WKInterfaceLabel!
+    @IBOutlet var flightNumberLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var planeImage: WKInterfaceImage!
+    
+    var flight: Flight? {
+        didSet {
+            if let flight = flight {
+                originLabel.setText(flight.origin)
+                destinationLabel.setText(flight.destination)
+                flightNumberLabel.setText(flight.number)
+                
+                if flight.onSchedule {
+                    statusLabel.setText("On Time")
+                } else {
+                    statusLabel.setText("Delayed")
+                    statusLabel.setTextColor(UIColor.redColor())
+                }
+            }
+        }
+    }
+    
+}
